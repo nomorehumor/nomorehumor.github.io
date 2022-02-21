5df66527b7b87e8188024da25a40aae1ef25864f
CAS\Maxim.Popov <maxim.popov@cas.de>
2022-02-21 11:23:04 +0100

    Updates

---
+++ precache-manifest.43945d0cf8c3855fdcac70a2de20d2c6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c1a84ee2732748c8a8d4374d80a39b6",
+    "url": "/City-Navigator/index.html"
+  },
+  {
+    "revision": "f25158c761663b93641b",
+    "url": "/City-Navigator/static/css/main.7acebc09.chunk.css"
+  },
+  {
+    "revision": "23b6e24308311d212e04",
+    "url": "/City-Navigator/static/js/2.7f3b29dc.chunk.js"
+  },
+  {
+    "revision": "f25158c761663b93641b",
+    "url": "/City-Navigator/static/js/main.4b72af75.chunk.js"
+  },
+  {
+    "revision": "7235ad3cecdd052204ac",
+    "url": "/City-Navigator/static/js/runtime~main.74326330.js"
+  }
+]);
